326646e3bb53e01d67be3ecd05724575eccba615
Ken-S-0114 <satoken@satoukennoMacBook-Pro.local>
2017-05-09 04:22:08 +0900

    Initial Commit

---
+++ MyQuestions/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyQuestions
+//
+//  Created by 佐藤賢 on 2017/05/09.
+//  Copyright © 2017年 佐藤賢. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
